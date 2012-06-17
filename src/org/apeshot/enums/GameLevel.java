00461c47e9070e911ae22fa75b54bc9b2faf5c74
Saman Sedighi Rad <saman.sr@gmail.com>
2012-06-17 19:02:57 +0200

    Removing old renamed packages

--- src/org/apeshot/enums/GameLevel.java
+++
@@ -1,5 +0,0 @@
-package org.apeshot.enums;
-
-public enum GameLevel {
-	LEVEL_ONE, LEVEL_TWO
-}
--- src/org/ssrad/apeshot/interfaces/IExplodeable.java
+++
@@ -1,9 +0,0 @@
-package org.ssrad.apeshot.interfaces;
-
-public interface IExplodeable {
-	
-	public boolean isExplodable();
-	
-	public void onExplode();
-
-}
